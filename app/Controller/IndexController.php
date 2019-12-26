a45190b8d65cbb68902e06cce825dab952194e7b
xuyongjun <xuyongjun84@163.com>
2019-12-26 15:34:46 +0800

    #

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of Hyperf˙.
  *
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
@@ -11,17 +11,34 @@ declare(strict_types=1);
  */
 
 namespace App\Controller;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\HttpServer\Annotation\AutoController;
 
+/**
+ * Class IndexController
+ * @package App\Controller
+ * @AutoController()
+ */
 class IndexController extends AbstractController
 {
+    /**
+     * @Inject()
+     * @var \Hyperf\Contract\SessionInterface
+     */
+    private $session;
+
     public function index()
     {
         $user = $this->request->input('user', 'Hyperf');
         $method = $this->request->getMethod();
-
+        $this->session->set("welcome", "back");
         return [
             'method' => $method,
             'message' => "Hello {$user}.",
         ];
+    }
+
+    public function session(){
+        return $this->session->get("welcome");
     }
 }
